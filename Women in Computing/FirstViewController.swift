7faa9920b2915af604ce9d0a08956a8a271b1b53
alekya pochampally <s536776@nwmissouri.edu>
2020-02-26 16:37:17 -0600

    intial project setup

---
+++ Women in Computing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Women in Computing
+//
+//  Created by student on 2/26/20.
+//  Copyright © 2020 Alekya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Women in Computing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Women in Computing
+//
+//  Created by student on 2/26/20.
+//  Copyright © 2020 Alekya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
