f862bd46fe5ee8fc107f327586c06de688bf8126
Manisha <S537360@nwmissouri.edu>
2020-04-01 20:34:21 -0400

    Menu view controller

---
+++ Women in Computing/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Women in Computing
+//
+//  Created by student on 4/1/20.
+//  Copyright © 2020 Alekya. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
